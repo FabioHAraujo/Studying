30cc810766810002a90c8f6874e3d3f45cff7d81
Fábio Henrique Araújo <contato@fabioharaujo.com.br>
2023-01-25 22:55:35 -0300

    java

---
+++ Java/sojava/src/java5/Fatorial.java
@@ -0,0 +1,12 @@
+package java5;
+
+public class Fatorial {
+	public static void main(String[] args) {
+		int n=1;
+		for (n=1;n<=10;n++) {
+			while(n>1) {
+				n = n*
+			}
+		}
+	}
+}
---
+++ Java/sojava/src/java5/TestaMultiplos.java
@@ -0,0 +1,11 @@
+package java5;
+
+public class TestaMultiplos {
+	public static void main(String[] args) {
+		int numero=1;
+		for (numero=1;numero<=100;numero++)
+			if (numero%3==0) {
+				System.out.println(numero);
+			}
+	}
+}
