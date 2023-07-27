9dcec25d430521f24e9c62f34e0dd5a9ca6b7011
Fábio Henrique Araújo <contato@fabioharaujo.com.br>
2023-07-27 16:27:55 -0300

    Atualizando

---
+++ React JS/ts/orgs-cesta/src/mocks/cesta.js
@@ -0,0 +1,16 @@
+import logo from '../../assets/logo.png';
+
+const cesta = {
+    topo: {
+        titulo: 'Detalhes da cesta',
+    },
+    detalhes: {
+        nome: 'Cesta de Verduras',
+        logoFazenda: logo,
+        fazenda: 'Jenny Jack Farm',
+        descricao: 'Uma cesta com produtos selecionados cuidadosamente. Da fazenda direto para sua cozinha',
+        preco: 'R$30,00',
+    },
+}
+
+export default cesta;
---
+++ React JS/ts/orgs-cesta/src/views/Cesta/componentes/Detalhes.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import { Image, Text, View, StyleSheet } from 'react-native';
+
+import Texto from '../../../componentes/Texto';
+import logo from '../../../../assets/logo.png';
+
+
+export default function Detalhes({nome, preco, logoFazenda, fazenda, descricao}){
+    return <>
+        <Texto style={estilos.nome}>{nome}</Texto>
+        <View style={estilos.fazenda}>
+            <Image source={logo} style={estilos.imagemFazenda} />
+            <Texto style={estilos.nomeFazenda}>Jenny Jack Farm</Texto>
+        </View>
+        <Texto style={estilos.descricao}>Uma cesta com produtos selecionados cuidadosamente. Da fazenda direto para sua cozinha</Texto>
+        <Texto style={estilos.preco}>{preco}</Texto>
+    </>
+}
+
+const estilos = StyleSheet.create({
+    nome: {
+        fontSize: 26,
+        lineHeight: 42,
+        fontWeight: 'bold',
+    },
+    fazenda: {
+        flexDirection: 'row',
+        paddingVertical: 12,
+    },
+    imagemFazenda: {
+        width: 32,
+        height: 32,
+    },
+    nomeFazenda: {
+        fontSize: 16,
+        lineHeight: 26,
+        marginLeft: 12,
+        fontFamily: 'MontserratRegular',
+    },
+    descricao: {
+        color: '#A3A3A3',
+        fontSize: 16,
+        lineHeight: 26,
+    },
+    preco: {
+        color: '#2A9F85',
+        fontWeight: 'bold',
+        fontSize: 26,
+        lineHeight: 42,
+        marginTop: 8,
+    },
+})
