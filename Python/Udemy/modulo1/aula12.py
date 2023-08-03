071aa141dc2055484521ae13063c69f4e8848d0d
Fábio Henrique Araújo <contato@fabioharaujo.com.br>
2023-08-03 09:35:28 -0300

    Update Curso

---
+++ Python/Udemy/modulo1/aula12.py
@@ -0,0 +1,10 @@
+# Exercicio - Calculo de IMC
+nome = "Fábio Henrique"
+altura = 1.70
+peso = 93
+imc = peso / altura**2
+
+print("Nome:", nome)
+print("Altura:", altura, "metros")
+print("Peso:", peso, "kg")
+print("IMC de:", imc)
